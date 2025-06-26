c407894469cbce468a35f594d9ecca59e4ec91e9
0xKev <56137695+0xKev@users.noreply.github.com>
2025-06-25 23:15:40 -0400

    2 tab indent for yml,yaml

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -18,9 +18,20 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "yaml", "yml" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.expandtab = true
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
     pattern = "*",
     callback = function()
-        if vim.bo.filetype ~= "go" and vim.bo.filetype ~= "make" then
+        local ft = vim.bo.filetype
+        if ft ~= "go" and ft ~= "make" and ft ~= "yaml" and ft ~= "yml" then
             vim.opt_local.tabstop = 4
             vim.opt_local.shiftwidth = 4
             vim.opt_local.softtabstop = 4

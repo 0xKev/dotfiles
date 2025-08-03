ed11a513ed1d1e24996af5324cdc0ccdaf2cd997
0xKev <56137695+0xKev@users.noreply.github.com>
2025-08-04 01:21:15 +0800

    wezterm unicode font issues

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,15 +4,18 @@ local config = wezterm.config_builder()
 
 -- You can specify some parameters to influence the font selection;
 -- for example, this selects a Bold, Italic font variant.
-config.font = wezterm.font("JetBrains Mono", { weight = "Regular" })
+config.font = wezterm.font_with_fallback({
+  "JetBrainsMono Nerd Font",     -- Main monospace font
+  "Noto Sans Devanagari",        -- For Devanagari
+  "Noto Sans CJK JP",            -- For Japanese
+  "Noto Sans",                   -- For Latin/Cyrillic
+  "Noto Color Emoji",            -- For full emoji support
+  "Symbols Nerd Font",           -- For box drawing, arrows, etc
+})
 
-config.font_rules = {
-	{
-		italic = true,
-		font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true }),
-	},
-}
-config.window_background_opacity = 0.97
+config.allow_square_glyphs_to_overflow_width = "Never"
+
+config.window_background_opacity = 1
 config.window_decorations = "RESIZE"
 
 config.color_scheme = "Catppuccin Mocha"
@@ -21,7 +24,7 @@ config.color_scheme = "Catppuccin Mocha"
 config.use_dead_keys = false
 
 -- logging keypress
-config.debug_key_events = true
+config.debug_key_events = false
 
 config.set_environment_variables = {}
 

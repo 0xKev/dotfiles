1f92ad9b421a5c4c69cde68e23b6d08dd56c78e1
0xKev <56137695+0xKev@users.noreply.github.com>
2026-02-13 04:59:54 -0500

    fix: nvim double terminal opening due to snacks/lazyvim keybindings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Only added this due to a bug where nvim spawns a child terminal in the parent terminal due to the root directory and first terminal directory mismatch
 vim.keymap.set("t", "<C-/>", "<cmd>hide<cr>", { desc = "Hide terminal", silent = true })
+vim.keymap.set("t", "<C-_>", "<cmd>hide<cr>", { desc = "Hide terminal", silent = true })
